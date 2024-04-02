2a4caf298ccebeb67fbf64e27ddf3a935fcbce52
Adarsh KS <adarshstillalive@gmail.com>
2024-04-02 14:02:53 +0530

    Initialisation commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function dropmenu(){
+  const menu=document.querySelector(".hammenulinks");
+  const icon=document.querySelector(".hammenuicon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
